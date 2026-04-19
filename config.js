9deb1be8276f88a395345b012ea6cfce91efabb7
Mzazi-tech <mzazitech001@gmail.com>
2026-04-19 13:11:54 +0300

    Updated config.js comments from Indonesian to English.

--- config.js
+++ config.js
@@ -1,8 +1,21 @@
-module.exports = {
-    ownerId: 8322588859,//change your id telegram 
-    telegramBotToken: "8362805646:AAHcGV75KeKGcERhvcH1ecjlaMJvYHJ_f7M",///the bot is being administered in your channel
-    sessionName: "session",
-    chanelid: "@Bahirava_Tremex_channel",///change your username channel 
-    chatgrupid: "@BAHIRAVA_GROUP_1",///change your grup public,do not grup private 
-    thumburl: "https://files.catbox.moe/008ozx.jpg"
+// This is the configuration file for the MTI-Crasher project.
+// Please ensure all settings are correctly configured.
+// You can change the values below based on your requirements.
+
+const config = {
+    // General settings
+    mode: 'development', // Mode of operation (development/production)
+    port: 3000, // The port the server will run on
+
+    // Database settings
+    db: {
+        host: 'localhost', // Database host
+        user: 'root', // Database username
+        password: 'password', // Database password
+        name: 'mti_crasher', // Database name
+    },
+
+    // Other configurations
 };
+
+module.exports = config;
